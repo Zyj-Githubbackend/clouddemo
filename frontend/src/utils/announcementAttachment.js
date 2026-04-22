18da3c36626571022aa8dc78d61960b45bde4d00
Zyj-Githubbackend <718949423@qq.com>
2026-04-22 23:05:09 +0800

    2026.4.22 k8s部署 测试

--- frontend/src/utils/announcementAttachment.js
+++
@@ -1,6 +0,0 @@
-export const buildAnnouncementAttachmentUrl = (attachmentKey, fileName = '') => {
-  if (!attachmentKey) return ''
-  const params = new URLSearchParams({ objectKey: attachmentKey })
-  if (fileName) params.set('fileName', fileName)
-  return `/api/announcement/attachment?${params.toString()}`
-}
--- frontend/src/utils/announcementImage.js
+++
@@ -1,9 +0,0 @@
-export const buildAnnouncementImageUrl = (imageKey) => {
-  if (!imageKey) return ''
-  return `/api/announcement/image?objectKey=${encodeURIComponent(imageKey)}`
-}
-
-export const buildAnnouncementImageUrls = (imageKeys = []) => {
-  if (!Array.isArray(imageKeys)) return []
-  return imageKeys.filter(Boolean).map(buildAnnouncementImageUrl)
-}
